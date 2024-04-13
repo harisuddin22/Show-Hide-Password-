3b57a7097637dffd97631dad301056cc335ff72e
harisuddin22 <160148739+harisuddin22@users.noreply.github.com>
2024-04-13 06:32:48 +0500

    Update 1.0

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-// window.onload = function() {
-//     let password = document.getElementById("search-bar");
-//     let seePassword = document.getElementById("show-button");
+window.onload = function() {
+    let password = document.getElementById("search-bar");
+    let seePassword = document.getElementById("show-button");
 
-//     function showPassword(){
-//         if (seePassword.innerText === "Show Password") {
-//             password.type = "text";
-//             seePassword.innerText = "Hide Password";
-//         } else {
-//             password.type = "password";
-//             seePassword.innerText = "Show Password";
-//         }
-//     }
-
-//     seePassword.addEventListener('click', showPassword);
-// }
-
-
-let button = document.getElementById('mode-switcher');
-let body = document.body;
-
-button.addEventListener('click', function() {
-    if (body.classList.contains('light-mode')) {
-        body.classList.remove('light-mode');
-        body.classList.add('dark-mode');
-    } else {
-        body.classList.remove('dark-mode');
-        body.classList.add('light-mode');
+    function showPassword(){
+        if (seePassword.innerText === "Show Password") {
+            password.type = "text";
+            seePassword.innerText = "Hide Password";
+        } else {
+            password.type = "password";
+            seePassword.innerText = "Show Password";
+        }
     }
-});
+
+    seePassword.addEventListener('click', showPassword);
+}
